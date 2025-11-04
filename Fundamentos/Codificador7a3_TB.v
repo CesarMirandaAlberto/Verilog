3849558adc28e774c41c9b5085f2d59e0fc8bf2c
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2025-11-03 21:37:53 -0600

    Testbench del módulo Codificador 7 a 3

--- Fundamentos/Codificador7a3_TB.v
+++ Fundamentos/Codificador7a3_TB.v
@@ -12,11 +12,11 @@
 `timescale 1ns/1ps
 //Declaración del módulo y su lista de señales acorde a los puertos del módulo a verificar
 module Codificador7a3_TB;
-	reg [6:0]X;
-	wire [2:0]Salida;
+	reg [6:0]X_TB;
+	wire [2:0]Salida_TB;
 
 	// instanciación y mapeo de señales del módulo Codificador
-	Codificador7a3 DUT(.X(X), .Salida(Salida));
+	Codificador7a3 DUT(.X(X_TB), .Salida(Salida_TB));
 
 	initial begin
 		// Parametros para archivos de simulación
@@ -25,17 +25,17 @@ module Codificador7a3_TB;
 		
 		//Mensaje en consola
 		$display("Iniciando simulacion.....");
-		$monitor("Tiempo: %t | X: %b | Salida: %b", $time, X, Salida);
+		$monitor("Tiempo: %t | X: %b | Salida: %b", $time, X_TB, Salida_TB);
 		
 		//Estimulos 	retardos
-		X = 7'b0000000; #10;
-		X = 7'b0000010; #10;
-		X = 7'b0000100; #10;
-		X = 7'b0001000; #10;
-		X = 7'b0010000; #10;
-		X = 7'b0100000; #10;
-		X = 7'b1000000; #10;
-		X = 7'b0000000; #10;
+		X_TB = 7'b0000000; #10;
+		X_TB = 7'b0000010; #10;
+		X_TB = 7'b0000100; #10;
+		X_TB = 7'b0001000; #10;
+		X_TB = 7'b0010000; #10;
+		X_TB = 7'b0100000; #10;
+		X_TB = 7'b1000000; #10;
+		X_TB = 7'b0000000; #10;
 		
 		$display("Fin de la simulacion...");
 		$finish; //fin de estimulos
