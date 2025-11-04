caac6dc9bcf85fd0677a8fd0d643d5064a4382b6
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2025-11-03 22:16:33 -0600

    Testbench de módulo Decodificador Binario a Hexadecimal

--- Fundamentos/DecodificadorBinario_Hexadecimal_TB.v
+++ Fundamentos/DecodificadorBinario_Hexadecimal_TB.v
@@ -13,11 +13,11 @@
 
 //Declaración del módulo y lista de puertos acorde a los del módulo a verificar
 module DecodificadorBinario_Hexadecimal_TB;
-		reg [3:0] Binario;
-		wire [6:0] Hexadecimal;
+		reg [3:0] Binario_TB;
+		wire [6:0] Hexadecimal_TB;
 	
 	//instanciación del módulo decodificador
-	DecodificadorBinario_Hexadecimal DUT(.Binario(Binario), .Hexadecimal(Hexadecimal));
+	DecodificadorBinario_Hexadecimal DUT(.Binario(Binario_TB), .Hexadecimal(Hexadecimal_TB));
 	
 	initial begin
 		//Parametros de archivos de simulacion
@@ -26,25 +26,25 @@ module DecodificadorBinario_Hexadecimal_TB;
 		
 		//Monitoreo de variables en consola
 		$display("Iniciando simulacion.....");
-		$monitor("Tiempo: %t | Binario: %b | Hexadecimal: %b", $time, Binario, Hexadecimal);
+		$monitor("Tiempo: %t | Binario: %b | Hexadecimal: %b", $time, Binario_TB, Hexadecimal_TB);
 		
 		//Estimulos       Retardos
-		Binario = 4'b0000; #10;
-		Binario = 4'b0001; #10;
-		Binario = 4'b0010; #10;
-		Binario = 4'b0011; #10;
-		Binario = 4'b0100; #10;
-		Binario = 4'b0101; #10;
-		Binario = 4'b0110; #10;
-		Binario = 4'b0111; #10;
-		Binario = 4'b1000; #10;
-		Binario = 4'b1001; #10;
-		Binario = 4'b1010; #10;
-		Binario = 4'b1011; #10;
-		Binario = 4'b1100; #10;
-		Binario = 4'b1101; #10;
-		Binario = 4'b1110; #10;
-		Binario = 4'b1111; #10;
+		Binario_TB = 4'b0000; #10;
+		Binario_TB = 4'b0001; #10;
+		Binario_TB = 4'b0010; #10;
+		Binario_TB = 4'b0011; #10;
+		Binario_TB = 4'b0100; #10;
+		Binario_TB = 4'b0101; #10;
+		Binario_TB = 4'b0110; #10;
+		Binario_TB = 4'b0111; #10;
+		Binario_TB = 4'b1000; #10;
+		Binario_TB = 4'b1001; #10;
+		Binario_TB = 4'b1010; #10;
+		Binario_TB = 4'b1011; #10;
+		Binario_TB = 4'b1100; #10;
+		Binario_TB = 4'b1101; #10;
+		Binario_TB = 4'b1110; #10;
+		Binario_TB = 4'b1111; #10;
 		
 		$display("Fin de la simulacion...");
 		$finish; // Fin
