e555b9d2221317d1e489fe609af697e630a65c13
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2025-11-03 22:09:22 -0600

    Testbench del módulo Demultiplexor 1 a 4

--- Fundamentos/Demultiplexor1a4_TB.v
+++ Fundamentos/Demultiplexor1a4_TB.v
@@ -12,15 +12,15 @@
 `timescale 1ns/1ps
 //Definición del módulo y lista de señales acorde al módulo a verificar
 module Demultiplexor1a4_TB;
-	reg [3:0] X;
-	reg [2:0] Selector;
-	wire [3:0] A;
-	wire [3:0] B;
-	wire [3:0] C;
-	wire [3:0] D;
+	reg [3:0] X_TB;
+	reg [2:0] Selector_TB;
+	wire [3:0] A_TB;
+	wire [3:0] B_TB;
+	wire [3:0] C_TB;
+	wire [3:0] D_TB;
 	
 	//instanciación
-	Demultiplexor1a4 DUT (.X(X), .Selector(Selector), .A(A), .B(B), .C(C), .D(D));
+	Demultiplexor1a4 DUT (.X(X_TB), .Selector(Selector_TB), .A(A_TB), .B(B_TB), .C(C_TB), .D(D_TB));
 	
 	initial begin
 		// Parametros de archivos de simulación
@@ -30,17 +30,17 @@ module Demultiplexor1a4_TB;
 		//Monitoreo de señales en consola
 		$display("Iniciado simulacion...");
 		$monitor("Tiempo: %t | X: %b | Selector: %b | A: %b | B: %b | C: %b | D: %b", 
-		$time, X, Selector, A, B, C, D);
+		$time, X_TB, Selector_TB, A_TB, B_TB, C_TB, D_TB);
 		
-		X = 4'b1011;
+		X_TB = 4'b1011;
 		
 	//	Estimulos        Retardos
-		Selector = 3'b000; #10;
-		Selector = 3'b001; #10;
-		Selector = 3'b010; #10;
-		Selector = 3'b011; #10;
-		Selector = 3'b100; #10;
-		Selector = 3'b101; #10;
+		Selector_TB = 3'b000; #10;
+		Selector_TB = 3'b001; #10;
+		Selector_TB = 3'b010; #10;
+		Selector_TB = 3'b011; #10;
+		Selector_TB = 3'b100; #10;
+		Selector_TB = 3'b101; #10;
 		
 		$display("Fin de la simulacion..");
 		$finish;
