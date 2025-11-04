595c8543119d6db79c9226f015951c502ec6f0a4
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2025-11-03 22:03:16 -0600

    Testbench del móduñp Demultiplexor 1 a 2

--- Fundamentos/Demultiplexor1a2_TB.v
+++ Fundamentos/Demultiplexor1a2_TB.v
@@ -12,13 +12,13 @@
 `timescale 1ns/1ps
 //Definición del módulo y lista de señales acorde a los puertos del módulo a verificar
 module Demultiplexor1a2_TB;
-		reg X;
-		reg [1:0] Selector;
-		wire Salida1;
-		wire Salida2;
+		reg X_TB;
+		reg [1:0] Selector_TB;
+		wire Salida1_TB;
+		wire Salida2_TB;
 		
 	//instanciación del módulo
-	Demultiplexor1a2 DUT(.X(X), .Selector(Selector), .Salida1(Salida1), .Salida2(Salida2));
+	Demultiplexor1a2 DUT(.X(X_TB), .Selector(Selector_TB), .Salida1(Salida1_TB), .Salida2(Salida2_TB));
 	
 	initial begin
 		//Parametros de los archivos de simulación
@@ -28,15 +28,15 @@ module Demultiplexor1a2_TB;
 		//Monitoreo de variables en consola
 		$display("Iniciando simulacion....");
 		$monitor("Tiempo:%0t | X: %b | Selector:%b | Salida1: %b | Salida2: %b",
-		$time, X, Selector, Salida1, Salida2);
+		$time, X_TB, Selector_TB, Salida1_TB, Salida2_TB);
 		
-		X = 1;
+		X_TB = 1;
 		
 		// Estimulos    Retardos
-		Selector = 2'b00; #10;
-		Selector = 2'b01; #10;
-		Selector = 2'b10; #10;
-		Selector = 2'b11; #10;
+		Selector_TB = 2'b00; #10;
+		Selector_TB = 2'b01; #10;
+		Selector_TB = 2'b10; #10;
+		Selector_TB = 2'b11; #10;
 		
 		$display("Fin de la simulacion..");
 		$finish;
