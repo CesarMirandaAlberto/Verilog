59a535b3e14dcd57d044e92120a5cffa7c0a1acd
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2026-03-06 22:35:18 -0600

    Archivo con los pines

---
+++ Miniproyectos/FSM Moore Semaforo 2 Vias/quartus/pins.tcl
@@ -0,0 +1,14 @@
+#ASIGNACIÓN DE PINES 
+
+#ENTRADAS
+set_location_assignment PIN_P11 -to CLK_50MHZ
+set_location_assignment PIN_C10 -to BTNRST
+
+#SALIDAS
+set_location_assignment PIN_A8 -to 	SEM1[0]
+set_location_assignment PIN_A9 -to 	SEM1[1]
+set_location_assignment PIN_A10 -to SEM1[2]
+
+set_location_assignment PIN_D14 -to SEM2[0]
+set_location_assignment PIN_A11 -to SEM2[1]
+set_location_assignment PIN_B11 -to SEM2[2]
