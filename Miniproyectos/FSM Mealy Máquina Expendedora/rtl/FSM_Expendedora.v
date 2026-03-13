bb5ba230bd1793037de2ce5ec2cdb382a93f3014
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2026-03-12 22:32:56 -0600

    Máquina de Estados Expendedora

--- Miniproyectos/FSM Mealy Máquina Expendedora/rtl/FSM_Expendedora.v
+++ Miniproyectos/FSM Mealy Máquina Expendedora/rtl/FSM_Expendedora.v
@@ -6,24 +6,24 @@ module FSM_Expendedora(
 	input BComprar,
 	output reg Entrega,
 	output reg Cambio,
-	output reg DAcumulado[7:0]
+	output reg [7:0]DAcumulado
 );
 
-	parameter S0 = 2'b00, S5 = 2'b01, S10 = 2'b10, S15 = 2'b11;
+	parameter S0 = 3'b000, S5 = 3'b001, S10 = 3'b010, S15 = 3'b011, S20 = 3'b100;
 	
 	parameter [1:0] CINCO = 2'b01;
 	parameter [1:0] DIEZ = 2'b10;
 	
-	reg [1:0] EstadoActual, SiguienteEstado; 
+	reg [2:0] EstadoActual, SiguienteEstado; 
 	
 	// ACTUALIZACIÓN DE ESTADOS
 	always@(posedge clk or posedge rst) begin
 		if (rst) begin
 			EstadoActual <= S0;
 		end 
-		else if (clk) begin
+		else begin
 			EstadoActual <= SiguienteEstado;
-		end if;
+		end
 		
 	end 
 	
@@ -31,44 +31,44 @@ module FSM_Expendedora(
 	always@(*) begin
 		SiguienteEstado = EstadoActual;
 		
-		if Enable = '0' begin
+		if(Enable == 1'b0)begin
 			SiguienteEstado = S0;
 		end
 		else begin
 			case(EstadoActual)
 				S0 : begin
-					if (Moneda = CINCO) begin
+					if (Moneda == CINCO) begin
 						SiguienteEstado = S5;
-					end;
-					else if (Moneda = DIEZ) begin
+					end
+					else if (Moneda == DIEZ) begin
 						SiguienteEstado = S10;
-					end;
+					end
 					else begin
-						SiguienteEstado <= S0;
-					end;
-				end;
+						SiguienteEstado = S0;
+					end
+				end
 				S5 : begin
-					if(Moneda = CINCO) begin
+					if(Moneda == CINCO) begin
 						SiguienteEstado = S10;
-					end;
-					else if(Moneda = DIEZ) begin
+					end
+					else if(Moneda == DIEZ) begin
 						SiguienteEstado = S15;
-					end;
+					end
 					else begin
 						SiguienteEstado = S5;
-					end;
-				end;
+					end
+				end
 				S10 : begin
-					if(Moneda = CINCO) begin
+					if(Moneda == CINCO) begin
 						SiguienteEstado = S15;
-					end;
-					else if(Moneda = DIEZ) begin
+					end
+					else if(Moneda == DIEZ) begin
 						SiguienteEstado = S20;
-					end;
+					end
 					else begin
 						SiguienteEstado = S10;
-					end;
-				end;
+					end
+				end
 				S15 : begin
 					if (BComprar) begin
 						SiguienteEstado = S0;
@@ -98,10 +98,10 @@ module FSM_Expendedora(
 			case (EstadoActual)
 				S15 : begin
 					if (BComprar) begin
-						Entrega = 1'b0;
+						Entrega = 1'b1;
 					end
 					else begin
-						Entrega = 1'b1;
+						Entrega = 1'b0;
 					end
 				end
 				
@@ -130,22 +130,22 @@ module FSM_Expendedora(
 			
 			S0 :  begin
 				DAcumulado = 8'b00000000;
-			end;
+			end
 			S5 : begin
 				DAcumulado = 8'b00000101;
-			end;
+			end
 			S10 : begin
 				DAcumulado = 8'b00001010;
-			end;
+			end
 			S15 : begin
 				DAcumulado = 8'b00001111;
-			end;
+			end
 			S20 : begin
 				DAcumulado = 8'b00010100;
-			end;
+			end
 			default : begin
 				DAcumulado = 8'b00000000;
-			end;
+			end
 		endcase;
 	end
 	
